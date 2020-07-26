c2fd22ef2589832d3dcd85c56eba4c2bc82ae12d
Alex Resiga <resiga.alex@gmail.com>
2020-07-27 01:16:38 +0300

    changes

---
+++ popup.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    currentyPlaying();
+    getLatest();
+});
+
+function getLatest() {
+    $.getJSON('http://tananana.ro/live/meta.php', function (data) {
+        console.log(data.latest);
+        $('div#main').text('');
+        $('div#main').append('<div class="card">' + data.current.split(' - ')[1] +'<span id="currently-playing"><i class="fa fa-volume-down" aria-hidden="true"></i></span><br><span>'+ data.current.split(' - ')[0] +'<span></div><br>');
+        data.latest.forEach(e => {
+            $('div#main').append('<div class="card">' + e.split(' - ')[1] +'<div>'+ e.split(' - ')[0] +'</div></div><br>');
+        });
+    });
+
+    setTimeout(getLatest, 240000); // statiscally chosen 4 minutes 
+}
+
+function currentyPlaying() {
+    const a = $('span#currently-playing');
+    a.html('<i class="fa fa-volume-up" aria-hidden="true"></i>');
+    setTimeout(function () {
+        a.html('<i class="fa fa-volume-down" aria-hidden="true"></i>');
+    }, 1500);
+
+    setInterval(currentyPlaying, 2500);
+}
+
+$('#spotify').click(function() {
+    console.log("btn");
+})
