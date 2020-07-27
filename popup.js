ec02d008f255eed4c7449088aa8fa8ec9b6d7577
Alex Resiga <resiga.alex@gmail.com>
2020-07-28 00:44:14 +0300

    fix promises

--- popup.js
+++ popup.js
@@ -1,20 +1,52 @@
-$(document).ready(function () {
+const CLIENT_ID = '5d9bc4f5c43c41548b24eb1161ebb589';
+const CLIENT_SECRET = 'b1c310e06b124c44b56141a9d5a24d39';
+let ACCESS_TOKEN = '';
+
+$(document).ready(async function () {
+    ACCESS_TOKEN = await getAccessToken();
     currentyPlaying();
     setInterval(currentyPlaying, 3000);
     getLatest();
+    // setInterval(getLatest, 9000); // refresh issues
 });
 
 function getLatest() {
-    $.getJSON('http://tananana.ro/live/meta.php', function (data) {
-        console.log(data.latest);
-        $('div#main').text('');
-        $('div#main').append('<div class="card">' + data.current.split(' - ')[1] +'<span id="currently-playing"><i class="fa fa-volume-up" aria-hidden="true"></i></span><br><span>'+ data.current.split(' - ')[0] +'<span></div><br>');
+    console.log('latest');
+    $.getJSON('http://tananana.ro/live/meta.php', async function (data) {
+        $('div#main').empty();
+        const track = await searchTrack(data.current);
+        const track_url = track.tracks.items[0]?.external_urls.spotify;
+        const url = track_url ? track_url : '#';
+        $('div#main').append('<div class="card"><a target="_blank" href="' + url + '">' + data.current.split(' - ')[1] + '</a><span id="currently-playing"><i class="fa fa-volume-up" aria-hidden="true"></i></span><br><span>' + data.current.split(' - ')[0] + '<span></div><br>');
+        let latest = [];
         data.latest.forEach(e => {
-            $('div#main').append('<div class="card">' + e.split(' - ')[1] +'<div>'+ e.split(' - ')[0] +'</div></div><br>');
+            latest.push(searchTrack(e));
+        });
+        Promise.all(latest).then((values) => {
+            console.log(values);
+            for (let i = 0; i < data.latest.length; ++i) {
+                const latestTrack = values[i];
+                const track_url = latestTrack.tracks.items[0]?.external_urls.spotify;
+                const url = track_url ? track_url : '#';
+                $('div#main').append('<div class="card"><a target="_blank" href="' + url + '">' + data.latest[i].split(' - ')[1] + '</a><div>' + data.latest[i].split(' - ')[0] + '</div></div><br>');
+            }
         });
     });
+}
 
-    setTimeout(getLatest, 240000); // statiscally chosen 4 minutes 
+function searchTrack(title) {
+    const encodedTitle = encodeURIComponent(title.split(' - ').join(' '));
+    return $.ajax({
+        url: "https://api.spotify.com/v1/search?q=" + encodedTitle + "&type=track",
+        type: "GET",
+        contentType: "application/x-www-form-urlencoded",
+        headers: {
+            "Authorization": "Bearer " + ACCESS_TOKEN.access_token
+        },
+        data: {
+            "grant_type": "client_credentials",
+        }
+    });
 }
 
 function currentyPlaying() {
@@ -25,6 +57,16 @@ function currentyPlaying() {
     }, 1500);
 }
 
-$('#spotify').click(function() {
-    console.log("btn");
-})
+function getAccessToken() {
+    return $.ajax({
+        url: "https://accounts.spotify.com/api/token",
+        type: "POST",
+        contentType: "application/x-www-form-urlencoded",
+        headers: {
+            "Authorization": "Basic " + btoa(CLIENT_ID + ":" + CLIENT_SECRET)
+        },
+        data: {
+            "grant_type": "client_credentials",
+        }
+    });
+}
