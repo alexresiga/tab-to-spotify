32c8469ec45eb1157aa0e3226d7ba8db156ee2ca
Alex Resiga <resiga.alex@gmail.com>
2020-07-27 22:27:32 +0300

    fix animation

--- popup.js
+++ popup.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
     currentyPlaying();
+    setInterval(currentyPlaying, 3000);
     getLatest();
 });
 
@@ -7,7 +8,7 @@ function getLatest() {
     $.getJSON('http://tananana.ro/live/meta.php', function (data) {
         console.log(data.latest);
         $('div#main').text('');
-        $('div#main').append('<div class="card">' + data.current.split(' - ')[1] +'<span id="currently-playing"><i class="fa fa-volume-down" aria-hidden="true"></i></span><br><span>'+ data.current.split(' - ')[0] +'<span></div><br>');
+        $('div#main').append('<div class="card">' + data.current.split(' - ')[1] +'<span id="currently-playing"><i class="fa fa-volume-up" aria-hidden="true"></i></span><br><span>'+ data.current.split(' - ')[0] +'<span></div><br>');
         data.latest.forEach(e => {
             $('div#main').append('<div class="card">' + e.split(' - ')[1] +'<div>'+ e.split(' - ')[0] +'</div></div><br>');
         });
@@ -18,12 +19,10 @@ function getLatest() {
 
 function currentyPlaying() {
     const a = $('span#currently-playing');
-    a.html('<i class="fa fa-volume-up" aria-hidden="true"></i>');
+    a.html('<i class="fa fa-volume-down" aria-hidden="true"></i>');
     setTimeout(function () {
-        a.html('<i class="fa fa-volume-down" aria-hidden="true"></i>');
+        a.html('<i class="fa fa-volume-up" aria-hidden="true"></i>');
     }, 1500);
-
-    setInterval(currentyPlaying, 2500);
 }
 
 $('#spotify').click(function() {
