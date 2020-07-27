8372bb6193e9879a8536d71945f6c0fa65830eae
Alex Resiga <resiga.alex@gmail.com>
2020-07-28 02:53:00 +0300

    refactor to use createElement

--- popup.js
+++ popup.js
@@ -7,35 +7,66 @@ $(document).ready(async function () {
     currentyPlaying();
     setInterval(currentyPlaying, 3000);
     getLatest();
-    // setInterval(getLatest, 9000); // refresh issues
 });
 
 function getLatest() {
-    console.log('latest');
     $.getJSON('http://tananana.ro/live/meta.php', async function (data) {
         $('div#main').empty();
-        const track = await searchTrack(data.current);
-        const track_url = track.tracks.items[0]?.external_urls.spotify;
-        const url = track_url ? track_url : '#';
-        $('div#main').append('<div class="card"><a target="_blank" href="' + url + '">' + data.current.split(' - ')[1] + '</a><span id="currently-playing"><i class="fa fa-volume-up" aria-hidden="true"></i></span><br><span>' + data.current.split(' - ')[0] + '<span></div><br>');
+        const current = await createTrackElement(data.current, true);
+        $('div#main').append(current);
+        // $('div#main').append('<div class="card"><a target="_blank" ' + url + '">' + data.current.split(' - ')[1] + '</a><span id="currently-playing"><i class="fa fa-volume-up" aria-hidden="true"></i></span><br><span>' + data.current.split(' - ')[0] + '<span></div><br>');
         let latest = [];
         data.latest.forEach(e => {
             latest.push(searchTrack(e));
         });
-        Promise.all(latest).then((values) => {
+        Promise.all(latest).then(async (values) => {
             console.log(values);
             for (let i = 0; i < data.latest.length; ++i) {
-                const latestTrack = values[i];
-                const track_url = latestTrack.tracks.items[0]?.external_urls.spotify;
-                const url = track_url ? track_url : '#';
-                $('div#main').append('<div class="card"><a target="_blank" href="' + url + '">' + data.latest[i].split(' - ')[1] + '</a><div>' + data.latest[i].split(' - ')[0] + '</div></div><br>');
+                const elem = await createTrackElement(data.latest[i], false);
+                $('div#main').append(elem);
+                // $('div#main').append('<div class="card"><a target="_blank" ' + url + '">' + data.latest[i].split(' - ')[1] + '</a><div>' + data.latest[i].split(' - ')[0] + '</div></div>');
             }
         });
     });
 }
 
+async function createTrackElement(raw, isCurrent) {
+    const track = await searchTrack(raw);
+    const trackDiv = document.createElement('div');
+    trackDiv.className = "card";
+    const spotifyAnchor = document.createElement('a');
+    if (getSpotifyURL(track)) {
+        spotifyAnchor.setAttribute('href', getSpotifyURL(track));
+    }
+    const title = document.createTextNode(raw.split(' - ')[1]);
+    spotifyAnchor.appendChild(title);
+    trackDiv.appendChild(spotifyAnchor);
+    if (isCurrent) {
+        const currentyPlayingSpan = document.createElement('span');
+        currentyPlayingSpan.id = "currently-playing";
+        const volumeIcon = document.createElement('i');
+        volumeIcon.classList.add('fa');
+        volumeIcon.classList.add('fa-volume-up');
+        volumeIcon.setAttribute('aria-hidden', "true");
+        currentyPlayingSpan.appendChild(volumeIcon);
+        trackDiv.appendChild(currentyPlayingSpan);
+    }
+    const breakLine = document.createElement('br');
+    trackDiv.appendChild(breakLine);
+    const artistSpan = document.createElement('span');
+    const artist = document.createTextNode(raw.split(' - ')[0]);
+    artistSpan.appendChild(artist);
+    trackDiv.appendChild(artistSpan);
+    return trackDiv;
+}
+
+function getSpotifyURL(track) {
+    return track.tracks.items[0]?.external_urls.spotify ? track.tracks.items[0]?.external_urls.spotify : '';
+}
+
 function searchTrack(title) {
-    const encodedTitle = encodeURIComponent(title.split(' - ').join(' '));
+    console.log(title);
+    const encodedTitle = encodeURIComponent(title.split(' FT. ')[0].split(' - ').join(' ')); // can be improved for 're vs are etc
     return $.ajax({
         url: "https://api.spotify.com/v1/search?q=" + encodedTitle + "&type=track",
         type: "GET",
